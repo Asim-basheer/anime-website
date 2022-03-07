a4f38e6399ccf7de730549d62c0a5f98846ad63c
abc <abc@example.com>
2022-03-07 06:02:19 +0300

    give a slider life and edit its responsive

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,54 +1,80 @@
+import { useState } from "react";
 import { MdArrowBackIosNew } from "react-icons/md";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
 import { Link } from "react-router-dom";
-
+import { SliderData } from "./SliderData";
 import "./slider.scss";
 function Slider() {
+  const [current, setCurrent] = useState(1);
+
+  const nextAnime = () => {
+    setCurrent(current === SliderData.length - 1 ? 0 : current + 1);
+  };
+  const prvAnime = () => {
+    setCurrent(current === 0 ? SliderData.length - 1 : current - 1);
+  };
+
+  const bulletsHandler = (index) => {
+    setCurrent(index);
+  };
+
+  setInterval(() => {}, 5000);
   return (
-    <div
-      className="slider"
-      style={{
-        backgroundImage:
-          "url(https://www13.0zz0.com/2022/03/06/02/700270706.jpg)",
-      }}
-    >
-      <div className="slider-content">
-        <div className="slider-heading">
-          <h1>
-            Demon Slayer
-            <br />
-            <span>Kimetsu no Yaiba: Yuukaku-hen</span>
-          </h1>
-          <p>
-            Lorem feugait et volutpat. ut consequat. dolore tation ea iriure
-            zzril dolore nonummy tincidunt amet, minim laoreet accumsan euismod
-            blandit nulla te consequat, in molestie
-          </p>
-          <Link to={""} className="btn btn-watch">
-            Watch now
-          </Link>
-        </div>
-        <div className="slider-anime-img">
-          <img
-            src="https://www14.0zz0.com/2022/03/04/02/741595424.jpg"
-            alt="anime img"
-          />
-        </div>
-      </div>
+    <div className="slider">
+      {SliderData.map((data, index) => {
+        return (
+          <div
+            key={data.id}
+            className={
+              current === index ? "slider-content active" : "slider-content"
+            }
+            style={{
+              backgroundImage: `url(${data.img_url_wall})`,
+            }}
+          >
+            <div className="slider-heading">
+              <h1>
+                {data.title}
+                <br />
+                <span>
+                  {data.title_discription.length > 30
+                    ? data.title_discription.slice(0, 20) + "..."
+                    : data.title_discription}
+                </span>
+              </h1>
+              <p>
+                {data.discription.length > 200
+                  ? data.discription.slice(0, 210) + "..."
+                  : data.discription}
+              </p>
+              <Link to={`/details/${data.id}`} className="btn btn-watch">
+                Watch now
+              </Link>
+            </div>
+            <div className="slider-anime-img">
+              <img src={data.img_url} alt="anime img" />
+            </div>
+          </div>
+        );
+      })}
       <div className="slider-control">
         <div className="slider-bullets">
-          <span className="active"></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          {SliderData.map((data, index) => {
+            return (
+              <span
+                key={data.id}
+                className={current === index ? "active" : ""}
+                onClick={() => bulletsHandler(index)}
+              ></span>
+            );
+          })}
         </div>
         <div className="slider-arrows">
-          <div className="arrow-next">
-            <MdArrowBackIosNew />
-          </div>
           <div className="arrow-previous">
-            <MdOutlineArrowForwardIos />
+            <MdArrowBackIosNew onClick={prvAnime} />
+          </div>
+          <div className="arrow-next">
+            <MdOutlineArrowForwardIos onClick={nextAnime} />
           </div>
         </div>
       </div>
---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,48 @@
+export const SliderData = [
+  {
+    id: 1,
+    img_url_wall: "https://www8.0zz0.com/2022/03/06/22/788540469.jpg",
+    img_url: "https://www9.0zz0.com/2022/03/06/22/431426694.jpg",
+    title: "Leadale no Daichi nite",
+    title_discription: "In the Land of Leadale",
+    discription: `The previously bedridden Keina Kagami finds herself in the world of her favorite VRMMORPG—titled Leadale—after the life support keeping her alive fails. Reincarnated in the body of her high-level character, "Cayna," she notices that the world of Leadale is different from what she remembers playing.
+    As she comes to learn, two hundred years have passed since she last interacted with the world. However, this does not sadden Cayna—as it means that a new journey awaits her, filled with exciting prospects and unfamiliar faces all waiting to be discovered.`,
+  },
+  {
+    id: 2,
+    img_url_wall: "https://www8.0zz0.com/2022/03/06/22/863281511.jpg",
+    img_url: "https://www9.0zz0.com/2022/03/06/22/975892724.jpg",
+    title: "Akebi-chan no Sailor-fuku",
+    title_discription: "Akebi's Sailor Uniform",
+    discription: `Ever since she was young, Komichi Akebi has always adored sailor uniforms, even going so far as to ask her mother to sew one if she succeeds in getting into her mother's alma mater, Roubai Academy. And thus, when she gets accepted into the prestigious school, Komichi is ecstatic. However, much to her surprise, the middle school no longer uses sailor uniforms as its dress code—making Komichi stand out from her schoolmates. Despite this, Komichi is granted permission to continue wearing the traditional attire.
+    With renewed confidence, Komichi meets fascinating classmates as they experience school life together. Under the colorful shower of blossoming prospects, an exciting tomorrow awaits them!`,
+  },
+  {
+    id: 3,
+    img_url_wall: "https://www8.0zz0.com/2022/03/06/22/622778114.jpg",
+    img_url: "https://www9.0zz0.com/2022/03/06/22/512732364.jpg",
+    title: "Vanitas no Karte Part 2",
+    title_discription: "The Case Study of Vanitas",
+    discription: `Vanitas and Noé Archiviste head out to the town of Gévaudan in search of the "Beast," an enormous wolf-like creature that has slaughtered hundreds of people. Suspecting that the Beast is a curse-bearing vampire, Vanitas primarily aims to heal it using the powers of his grimoire.
+    Along the way, the two get separated and suddenly travel back to the past—to the exact moment the Beast is lurking in the woods. After a battle against the gigantic wolf and a vampire hunter, Vanitas decides to team up with Jeanne in order to find Noé. Despite being allies, Jeanne's goal is the opposite of Vanitas', as she was tasked to kill the Beast—suspecting it may be someone she used to know.`,
+  },
+  {
+    id: 4,
+    img_url_wall: "https://www8.0zz0.com/2022/03/06/22/904984130.jpg",
+    img_url: "https://www9.0zz0.com/2022/03/06/22/367873709.jpg",
+    title: "Kimetsu no Yaiba: Yuukaku-hen",
+    title_discription:
+      "Demon Slayer: Kimetsu no Yaiba Entertainment District Arc",
+    discription: `
+    The devastation of the Mugen Train incident still weighs heavily on the members of the Demon Slayer Corps. Despite being given time to recover, life must go on, as the wicked never sleep: a vicious demon is terrorizing the alluring women of the Yoshiwara Entertainment District. The Sound Pillar, Tengen Uzui, and his three wives are on the case. However, when he soon loses contact with his spouses, Tengen fears the worst and enlists the help of Tanjirou Kamado, Zenitsu Agatsuma, and Inosuke Hashibira to infiltrate the district's most prominent houses and locate the depraved Upper Rank demon.`,
+  },
+  {
+    id: 5,
+    img_url_wall: "https://www8.0zz0.com/2022/03/06/22/561178175.jpg",
+    img_url: "https://www9.0zz0.com/2022/03/06/22/373071359.jpg",
+    title: "Shingeki no Kyojin: The Final Season Part 2",
+    title_discription: "Attack on Titan: The Final Season Part 2",
+    discription: `Turning against his former allies and enemies alike, Eren Yeager sets a disastrous plan in motion. Under the guidance of the Beast Titan, Zeke, Eren takes extreme measures to end the ancient conflict between Marley and Eldia—but his true intentions remain a mystery. Delving deep into his family's past, Eren fights to control his own destiny.
+    Meanwhile, the long-feuding nations of Marley and Eldia utilize both soldiers and Titans in a brutal race to eliminate the other. Reiner Braun uses his own powers in a desperate bid to hold off Eren's own militaristic force, and his fellow Eldians—children Falco Grice and Gabi Braun—struggle to survive in the unfolding chaos.`,
+  },
+];
